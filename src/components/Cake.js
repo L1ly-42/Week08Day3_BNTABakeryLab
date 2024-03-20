bbcb037ff44ca10f5146325ae8cd53830b4ccc81
Leila Peltier <l_peltier@outlook.com>
2024-03-20 16:11:10 +0000

    fixed ingredient formatting issue

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,10 +1,9 @@
 const Cake = ({cakeName, ingredients, price, rating}) => {
 
-    
     return ( 
     <>
     <h3>{cakeName}</h3>
-    <p>Ingredients: {ingredients}</p>
+    <p>Ingredients: {ingredients.join(', ')}</p>
     <p> Price: {price}</p>
     <p> Rating: {rating}</p>
     </> 
