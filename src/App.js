b11bc2dc1b4ae01ece62d233e6e41b6b1936777b
Leila Peltier <l_peltier@outlook.com>
2024-03-20 16:00:38 +0000

    Cake Container MVP code complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CakeContainer from './containers/CakeContainer';
 
 function App() {
   return (
  <>
+ <h1>Bakery</h1>
+ <CakeContainer/>
  
  </>
   );
---
+++ src/containers/CakeContainer.js
@@ -0,0 +1,57 @@
+import Cake from "../components/Cake";
+
+const  CakeContainer= () => {
+
+const victoriaSponge = {
+    cakeName: "Victoria Sponge",
+       ingredients: [
+           "eggs",
+          "butter",
+          "sugar",
+              "self-raising flour",
+          "baking powder",
+          "milk"
+      ],
+      price: 5,
+       rating: 5
+}
+
+const teaLoaf = {
+    cakeName: "Tea Loaf",
+      ingredients: [
+          "eggs",
+         "oil",
+         "dried fruit",
+             "sugar",
+         "self-raising flour",
+         "strong tea",
+     ],
+     price: 2,
+     rating: 3
+}
+
+const carrotCake = {
+    cakeName: "Carrot Cake",
+	   	ingredients: [
+   		 	"carrots",
+   	   		"walnuts",
+	      	"oil",
+   		   	"cream cheese",
+   	   		"flour",
+	      	"sugar",
+   		],
+	   	price: 8,
+   		rating: 5
+}
+
+
+    return ( 
+        <>
+      <Cake cakeName={victoriaSponge.cakeName} ingredients={victoriaSponge.ingredients} price={victoriaSponge.price} rating={victoriaSponge.rating}/>
+      <Cake cakeName={teaLoaf.cakeName} ingredients={teaLoaf.ingredients} price={teaLoaf.price} rating={teaLoaf.rating}/>
+      <Cake cakeName={carrotCake.cakeName} ingredients={carrotCake.ingredients} price={carrotCake.price} rating={carrotCake.rating}/>
+        </>
+     );
+}
+ 
+export default CakeContainer;
