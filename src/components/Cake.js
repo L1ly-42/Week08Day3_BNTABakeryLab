0e0eaf2ea3776a61f517d9d7ae49fbe962c82eff
Leila Peltier <108727885+L1ly-42@users.noreply.github.com>
2024-03-21 09:27:22 +0000

    Update Cake.js
    Added a pound sign

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -9,7 +9,7 @@ const Cake = ({cakeName, ingredients, price, rating, count, onButtonClick}) => {
     <div>
     <h3>{cakeName}</h3>
     <p>Ingredients: {ingredients.join(', ')}</p>
-    <p> Price: {price}</p>
+    <p> Price: £{price}</p>
     <p> Rating: {rating}</p>
     <button onClick={handleRevenue}>Sell Cake</button>
     </div> 
